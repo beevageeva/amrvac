87cbce4b5d3c623ddcf6382f14a0e5ef26c1f578
beevageeva <beevageeva@users.noreply.github.com>
2023-01-11 09:16:04 +0000

    deploy 6c6b96a7baadf3d23da3fc1e1eb12950e8fdef2b: added doc for convert dat

--- search/pages_4.js
+++ search/pages_4.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['data_20file_20conversion_6298',['Data file conversion',['../md_doc_convert.html',1,'']]],
-  ['discretization_6299',['Discretization',['../md_doc_discretization.html',1,'']]],
+  ['data_20file_20conversion_6297',['Data file conversion',['../md_doc_convert.html',1,'']]],
+  ['discretization_6298',['Discretization',['../md_doc_discretization.html',1,'']]],
+  ['dump_20variables_20to_20dat_20files_6299',['Dump variables to dat files',['../md_doc_dat_convert.html',1,'']]],
   ['dust_20in_20mpi_2damrvac_6300',['Dust in MPI-AMRVAC',['../md_doc_dust.html',1,'']]]
 ];
